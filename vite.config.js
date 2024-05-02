a72a0393e0b08053ecf3179bebdcf76974ddf854
Viki <v.nekrasova.e@yandex.ru>
2024-05-02 16:07:46 +0400

    add handlebars

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+    plugins: [handlebars()],
     server: {
         port: 3000
     }
-});
+})
+
